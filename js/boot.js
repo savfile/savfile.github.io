5f9f9f1e4e1a1b04205cb5512da0d0281fcdf8ad
savfile <savfile@outlook.com>
2022-07-06 11:03:13 +0000

    Deploying to main from @ savfile/blog@535a650343b9b8a0ec706c5d207c2c0bc75d09d9 🚀

--- js/boot.js
+++ js/boot.js
@@ -1,1 +1,54 @@
-Fluid.boot={},Fluid.boot.registerEvents=function(){Fluid.events.billboard(),Fluid.events.registerNavbarEvent(),Fluid.events.registerParallaxEvent(),Fluid.events.registerScrollDownArrowEvent(),Fluid.events.registerScrollTopArrowEvent(),Fluid.events.registerImageLoadedEvent()},Fluid.boot.refreshPlugins=function(){if(Fluid.plugins.fancyBox(),Fluid.plugins.codeWidget(),"tocbot"in window){tocbot.refresh();var t=jQuery("#toc");if(0===t.length||!tocbot)return;0<t.find(".toc-list-item").length&&t.css("visibility","visible")}if("anchors"in window){anchors.removeAll();var e,n=[];for(e of(CONFIG.anchorjs.element||"h1,h2,h3,h4,h5,h6").split(","))n.push(".markdown-body > "+e.trim());"left"===CONFIG.anchorjs.placement&&(anchors.options.class="anchorjs-link-left"),anchors.add(n.join(", "))}"MathJax"in window&&MathJax.startup.document&&"function"==typeof MathJax.startup.document.state&&(MathJax.startup.document.state(0),MathJax.texReset(),MathJax.typeset(),MathJax.typesetPromise()),"mermaid"in window&&mermaid.init()},document.addEventListener("DOMContentLoaded",function(){Fluid.boot.registerEvents()});
+/* global Fluid */
+
+Fluid.boot = {};
+
+Fluid.boot.registerEvents = function() {
+  Fluid.events.billboard();
+  Fluid.events.registerNavbarEvent();
+  Fluid.events.registerParallaxEvent();
+  Fluid.events.registerScrollDownArrowEvent();
+  Fluid.events.registerScrollTopArrowEvent();
+  Fluid.events.registerImageLoadedEvent();
+};
+
+Fluid.boot.refreshPlugins = function() {
+  Fluid.plugins.fancyBox();
+  Fluid.plugins.codeWidget();
+
+  if ('tocbot' in window) {
+    tocbot.refresh();
+    var toc = jQuery('#toc');
+    if (toc.length === 0 || !tocbot) { return; }
+    if (toc.find('.toc-list-item').length > 0) {
+      toc.css('visibility', 'visible');
+    }
+  }
+
+  if ('anchors' in window) {
+    anchors.removeAll();
+    var el = (CONFIG.anchorjs.element || 'h1,h2,h3,h4,h5,h6').split(',');
+    var res = [];
+    for (var item of el) {
+      res.push('.markdown-body > ' + item.trim());
+    }
+    if (CONFIG.anchorjs.placement === 'left') {
+      anchors.options.class = 'anchorjs-link-left';
+    }
+    anchors.add(res.join(', '));
+  }
+
+  if ('MathJax' in window && MathJax.startup.document && typeof MathJax.startup.document.state === 'function') {
+    MathJax.startup.document.state(0);
+    MathJax.texReset();
+    MathJax.typeset();
+    MathJax.typesetPromise();
+  }
+
+  if ('mermaid' in window) {
+    mermaid.init();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  Fluid.boot.registerEvents();
+});
